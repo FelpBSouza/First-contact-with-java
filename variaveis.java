36b0fb8066901ca1a300daae3688d8656e6333b3
FelpBSouza <fsouzab13@hotmail.com>
2022-08-31 12:19:56 -0300

    Java Variables

---
+++ variaveis.java
@@ -0,0 +1,51 @@
+public class variaveis {
+
+      public static void main(String[] args) {
+                //Variaveis do tipo inteiro
+                //para declarar uma variavel eu defino qual ser o a variavel e logo em seguida atribuo um nome junto a um valor
+                //inteiro =byte<short<int<long
+                //byte vai de -128 ate 127
+                byte NomeMinusculo = 10;
+                //short vai de = -32768 ate 32767
+                short NomePequeno = 32767;
+                //int vai de  = -2147483648 ate 2147483647
+                int NomeGrande = -2147483648;
+                //long vai de  = -9223372036854775808 ate 9223372036854775807
+                long NomeGigante =9223372036854775807l;
+
+
+                //Variaveis do tipo Ponto Flutuante
+                float peso = 56.7f;
+                //numero grande depois da virgula
+                double pi = 3.14151231231231;
+
+                //Variaveis do tipo Caractere
+                char letra = 'J';
+
+                //Variavel do tipo Booleano (true or false) ->true (verdadeiro) false(falso)
+                boolean estoucomfome = true;
+
+                //Variavel do tipo String
+                //string = objeto
+                String nome = "Felipe";
+
+
+
+
+
+                System.out.println(NomeMinusculo);
+                System.out.println(NomePequeno);
+                System.out.println(NomeGrande);
+                System.out.println(NomeGigante);
+
+
+      }
+
+
+
+
+
+
+
+    
+}
