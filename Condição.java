28f2473d21e1339683f15b14fc589f43c939bfcf
FelpBSouza <fsouzab13@hotmail.com>
2022-08-31 19:42:34 -0300

    Java condition

---
+++ Condição.java
@@ -0,0 +1,15 @@
+public class Condição {
+    public static void main(String[] args) {
+        //Condições
+        int vida = 100;
+
+        if(vida == 100){
+            vida ++;
+            System.out.println("Minha vida eh igual a 100");
+        }else{
+            System.out.println("Minha vida não eh igual a 100!");
+        }
+
+}
+    
+}
