3dae2e8bc359759d504483d338eec00696051e7a
FelpBSouza <fsouzab13@hotmail.com>
2022-08-31 19:04:01 -0300

    Java Arrays

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+        //arrays é aonde vc pode colocar varias informações
+        String[] nome = new String[5];
+        nome[0] = "Felipe";
+        nome[1] = "Jane";
+
+        System.out.println("O nome eh " +nome[1]);
+}
+}
+    
+
