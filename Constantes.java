c2408959b8d3a80f6a7e478c065fd58422ca9a8d
FelpBSouza <fsouzab13@hotmail.com>
2022-08-31 12:57:04 -0300

    Constant Study

---
+++ Constantes.java
@@ -0,0 +1,25 @@
+public class Constantes {
+
+    public static void main(String[] args) {
+
+             //constantes é um valor q n vai mudar
+             final int vidamaxima =10;
+
+             System.out.println(vidamaxima);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
