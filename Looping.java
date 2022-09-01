cd0ac5944e6b7f825cb47722e70113e99d5f4546
FelpBSouza <fsouzab13@hotmail.com>
2022-09-01 12:25:24 -0300

    Study Java Loop

---
+++ Looping.java
@@ -0,0 +1,24 @@
+public class Looping {
+    public static void main(String[] args) {
+        //Loop
+        int contador = 0;
+        while(contador < 10){
+            System.out.println("Ola!+contador");
+            contador ++;
+        }
+
+        for (int i = 0; i <10;i++){
+            System.out.println("Ola"+i);
+        }
+        
+contador = 0
+        do{
+            //executa primeiro e depois verifica
+            System.out.println("Contador");
+            contador++;
+        
+        }while(contador < 3);
+
+   
+}
+}
