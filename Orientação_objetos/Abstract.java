dd5c3c88fe41b834e02628938c32114d76f46f05
FelpBSouza <fsouzab13@hotmail.com>
2022-09-02 13:09:11 -0300

    Java Abstract

---
+++ Orientação_objetos/Abstract.java
@@ -0,0 +1,8 @@
+package Orientação_objetos;
+
+abstract class Abstract {
+    //para eu add a classe abs na minha main eu tenho que add o extends e logo em seguida eu add o nome da minha classe
+    public void iniciarJogo(){
+
+    }
+}
