581b564e056fd5ea9fb95de4e4dee86d34ad7f9d
FelpBSouza <fsouzab13@hotmail.com>
2022-09-02 13:32:27 -0300

    Java Interface

---
+++ Orientação_objetos/main.java
@@ -0,0 +1,22 @@
+package Orientação_objetos;
+
+public class main implements playerinterface {
+public static void main(String[] args) {
+    
+}
+
+    
+public void iniciar(){
+    //todo auto-generated method stub
+}
+public void morrer(){
+    //todo auto-generated method stub
+}
+public void perderVida(){
+    //todo auto-generated method stub
+}
+
+
+    
+    
+}
---
+++ Orientação_objetos/playerinterface.java
@@ -0,0 +1,9 @@
+package Orientação_objetos;
+
+public interface playerinterface {
+
+    public abstract void iniciar();
+    public abstract void morrer();
+    public abstract void perderVida();
+    
+}
